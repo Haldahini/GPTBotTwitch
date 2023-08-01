8021adecb2cc3af7017cc8284542b4e1d58fb6e5
Haldahini <hugo@navily.com>
2023-08-01 12:18:28 +0200

    adding chat history per user to bot

--- src/messageHandler.ts
+++ src/messageHandler.ts
@@ -2,12 +2,22 @@ import tmiClient from '@/tmiClient'
 import { openAIApi } from '@/openAI'
 import type { ChatUserstate } from 'tmi.js'
 import { configs } from '@/helpers/config'
+import { ChatCompletionRequestMessage } from "openai";
+
+const usersMessages = new Map<string, Array<ChatCompletionRequestMessage>>
 
 export default (target: string, context: ChatUserstate, msg: string, self: boolean): void => {
     if (self) { return }
-    const asBotMention = msg.toLowerCase().includes(`@${configs.username}`)
+
+    const cleanMessage = msg.toLowerCase().replace(/([a-z]+) \1/g, (match, group) => group)
+    const asBotMention = cleanMessage.includes(`@${configs.username}`)
     const username = context.username ?? ''
+
     if (!asBotMention || username === '') { return }
+    const lastMessages = usersMessages.get(username) ?? []
+    lastMessages.push({ role: 'user', content: cleanMessage })
+
+    if (lastMessages.length > 6) { lastMessages.splice(0, 2) }
 
     (async () => {
         const chatCompletion = await openAIApi.createChatCompletion({
@@ -16,16 +26,17 @@ export default (target: string, context: ChatUserstate, msg: string, self: boole
             messages: [
                 {
                     role: 'system',
-                    content: `${configs.prePrompt} Tu dois repondre à : @${username}`
-                },
-                { role: 'user', content: msg }
-            ]
+                    name: configs.username,
+                    content: `${configs.prePrompt}`
+                }, ...lastMessages]
         })
 
         const botText = chatCompletion.data?.choices?.[0]?.message?.content
 
         if (botText !== undefined) {
             await tmiClient.say(target, botText)
+            lastMessages.push({role: 'system', content: botText})
+            usersMessages.set(username, lastMessages)
         }
     })().catch((e) => {
         console.log(e)
