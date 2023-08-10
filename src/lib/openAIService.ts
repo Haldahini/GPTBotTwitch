39c43c10941928f0818d3480c50808caf91ea91f
Haldahini <hugo@navily.com>
2023-08-10 16:43:34 +0200

    fix prompts and refactoring

--- src/lib/openAIService.ts
+++ src/lib/openAIService.ts
@@ -14,21 +14,37 @@ const configuration = new Configuration({
 const openAIService = new OpenAIApi(configuration)
 
 const getResumeOfLastNormalMessages = async (username: string, chatMessages: ChatMessage) => {
-    const resume = await openAIService.createCompletion({
+    const resume = await openAIService.createChatCompletion({
         model: 'gpt-3.5-turbo',
-        max_tokens: 75,
-        prompt: `En tant que ${process.env.USERNAME}, resume de la conversation suivante en un maximum de 500 caracteres.
-        ${chatMessages.resume.length > 0 ? `- Resumer precedemt : ${chatMessages.resume}, `: ``} - Conversation :
-        ${conversationToString(username, chatMessages.messages)}`
+        messages: [
+            {
+                role: "system",
+                content: `
+                #init 
+                - Tu dois résumer la conversation que te donne l'utilisateur en un maximum de 500 caracteres.
+                - L'utilisateur peux aussi mettre un resumer precedent et tu dois l'integrer au resumer.
+                - Tu dois garder les informations importantes qui peuvent permettre d'identifier l'utilisateur.
+                #rules
+                - Tu ne dois ecrire que le résumer de la conversation que te passe l'utilisateur.
+                `
+            },
+            {
+                role: "user",
+                content: `${chatMessages.resume.length > 0
+                    ? `# Resumer precedemt : ${chatMessages.resume}, `
+                    : ``
+                } 
+                # Conversation :
+                ${conversationToString(username, chatMessages.messages)}`
+            }
+        ]
     })
-
-    return resume.data?.choices?.[0]?.text
+    return resume.data?.choices?.[0]?.message?.content
 }
 
 const conversationToString = (username: string, messages: ChatCompletionRequestMessage[]) =>
     messages.reduce((prevMessage, message) =>
-            prevMessage + `> ${(message.role === 'system' ? process.env.USERNAME : username)}: ${message.content} ;`, '')
-
+            prevMessage + `- ${(message.role === 'system' ? process.env.USERNAME : username)}: ${message.content} \\n `, '')
 
 const converseWithOpenAI = async (username: string, chatMessage: ChatMessage) => {
     const chatCompletion = await openAIService.createChatCompletion({
@@ -38,20 +54,18 @@ const converseWithOpenAI = async (username: string, chatMessage: ChatMessage) =>
             {
                 role: 'system',
                 name: process.env.USERNAME,
-                content: `
-                    ${process.env.BOT_PREPROMPTS}, 
-                    - Tu parle avec ${username}.
-                    ${getResumeMessage(chatMessage)}
-                `
+                content: `${process.env.BOT_PREPROMPTS}, - Tu parle avec ${username}.\\n ${getResumeMessage(chatMessage)}`
             }, ...chatMessage.messages
         ]
     })
+
+    console.log(`${process.env.BOT_PREPROMPTS}, - Tu parle avec ${username}.\\n ${getResumeMessage(chatMessage)}`)
 
     return chatCompletion.data?.choices?.[0]?.message?.content
 }
 
 const getResumeMessage = (messages: ChatMessage) => (messages.resume.length > 0)
-    ? `- Voici le resumer de ta conversation : ${messages.resume}`
+    ? `- Voici le resumer de ta conversation precedente : ${messages.resume}`
     : ''
 
 export {
